c0182d11f56e051eaee7d6686adfcc8fa464a99c
KTN__ <kritagyadahal588@gmail.com>
2024-05-12 12:59:04 +0545

    Added touch and which function for easyness

--- pwsh $profile/Microsoft.PowerShell_profile.ps1
+++ pwsh $profile/Microsoft.PowerShell_profile.ps1
@@ -2,12 +2,19 @@ winfetch
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\plague.omp.json" | Invoke-Expression
 Import-Module -Name Terminal-Icons
 Import-Module PSReadLine
-Import-Module z
+# Import-Module z
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
+}
+function touch($file){
+  "" | Out-File $file -Encoding ASCII 
+}
+function which($name){
+  Get-Command $name | Select-Object -ExpandProperty Definition
 }
 function digclk {
  $scriptPath = Resolve-Path -Path 'C:\Github\Digital Clock\digclk.ps1'
