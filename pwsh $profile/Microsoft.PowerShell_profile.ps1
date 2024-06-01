62149fc5c6e67e414451e254c57ceb8df6809157
KTN__ <kritagyadahal588@gmail.com>
2024-06-01 12:09:35 +0545

    Configured komorebi.json to be modern

--- pwsh $profile/Microsoft.PowerShell_profile.ps1
+++ pwsh $profile/Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,6 @@ winfetch
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\plague.omp.json" | Invoke-Expression
 Import-Module -Name Terminal-Icons
 Import-Module PSReadLine
-# Import-Module z
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
