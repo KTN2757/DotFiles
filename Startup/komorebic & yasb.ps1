a435cbaee9d310c6255216e58ab6e92f3a273c3e
KTN2757 <kritagyadahal588@gmail.com>
2024-07-03 20:47:35 +0545

    Made startup script compatable with python 3.12

--- Startup/komorebic & yasb.ps1
+++ Startup/komorebic & yasb.ps1
@@ -1,4 +1,4 @@
-taskkill /f /im python.exe
+taskkill /f /im python3.12.exe
 taskkill /f /im komorebi.exe
 komorebic start --whkd
-start python "C:\Github\yasb\src\main.py" -WindowStyle Hidden
+start python "D:\Github\yasb\src\main.py" -WindowStyle Hidden
