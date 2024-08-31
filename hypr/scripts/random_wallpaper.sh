caf10551e79750313a6b919d7adfde7da3353289
kritagyadahal588@gmail.com <KTN2757>
2024-09-01 02:57:30 +0545

    Hypr init

---
+++ hypr/scripts/random_wallpaper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wallpaper=$(find ~/Pictures/Wallpapers/ -type f -name "*.jpg" -o -name "*.png" | shuf -n 1)
+swww img --transition-type wipe $wallpaper
+wal -i $wallpaper
---
+++ hypr/scripts/wallpapers_slideshow.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while true; do
+    wallpaper=$(find ~/Pictures/Wallpapers/ -type f -name "*.jpg" -o -name "*.png" | shuf -n 1)
+    swww img --transition-type wipe $wallpaper
+    wal -i $wallpaper > /dev/null
+    sleep 300
+done
