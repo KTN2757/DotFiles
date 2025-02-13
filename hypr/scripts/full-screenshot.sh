79a91e18195a7e02c980e5f5ee2ef17afdcaded7
kritagyadahal588@gmail.com <80688932+KTN2757@users.noreply.github.com>
2025-02-13 18:45:28 +0545

    Some hotkeys and script updates

--- hypr/scripts/full-screenshot.sh
+++ hypr/scripts/full-screenshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-grim "$HOME/Pictures/Screenshots/"$(date +%Y%m%d-%H%M%S)"".png
+grim "$HOME/Pictures/Screenshots/"$(date +%Y-%m-%d_%H:%M:%S)"".png
 wl-copy < $(stat --format='%Y %n' $HOME/Pictures/Screenshots/* | sort -nr | head -n 1 | cut -d ' ' -f 2-)
