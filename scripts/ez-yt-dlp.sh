11af7d88e8c32bc735784da5983a693244ed2269
KTN2757 <80688932+KTN2757@users.noreply.github.com>
2026-04-13 21:35:46 +0545

    I deleted windows so updated the ez-yt-dlp script, NO WINDOWS RAHHH

--- scripts/ez-yt-dlp.sh
+++ scripts/ez-yt-dlp.sh
@@ -3,14 +3,12 @@ if [ $# -eq 3 ]; then
     yt-dlp "$1" -o "foo.webm" --cookies-from-browser firefox
     ffmpeg -i foo.webm "$2.mp3"
     mv "./$2.mp3" "$HOME/Music/$3"
-    cp "$HOME/Music/$3/$2.mp3" "/mnt/Windows/Users/Kritagya Dahal/Music/$3"
     rm "foo.webm"
 fi
 if [ $# -eq 4 ]; then
     yt-dlp "$1" -o "foo.webm" --cookies-from-browser firefox
     ffmpeg -i foo.webm "$2.$3"
     mv "./$2.$3" "$HOME/Music/$4"
-    cp "$HOME/Music/$4/$2.$3" "/mnt/Windows/Users/Kritagya\ Dahal/Music/$4"
     rm "foo.webm"
 fi
 if [ $# -ne 3 ] && [ $# -ne 4 ]; then
