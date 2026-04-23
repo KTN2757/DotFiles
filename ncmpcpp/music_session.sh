fef7a7b188b2892f196009e2c898bfbe95f0531e
KTN2757 <80688932+KTN2757@users.noreply.github.com>
2026-04-23 12:59:49 +0545

    Quick fix

--- ncmpcpp/music_session.sh
+++ ncmpcpp/music_session.sh
@@ -11,9 +11,6 @@ tmux split-window -v
 
 tmux select-layout "7902,171x44,0,0{85x44,0,0[85x16,0,0,15,85x27,0,17,18],85x44,86,0[85x22,86,0,16,85x21,86,23,17]}"
 
-tmux select-pane -t 1
-tmux send-keys "~/.config/ncmpcpp/cover.sh" C-m
-
 tmux select-pane -t 2
 tmux send-keys "ncmpcpp" C-m
 sleep 0.5
@@ -26,5 +23,8 @@ tmux send-keys "\\"
 
 tmux select-pane -t 4
 tmux send-keys "lyse" C-m
+
+tmux select-pane -t 1
+tmux send-keys "~/.config/ncmpcpp/cover.sh" C-m
 
 tmux attach -t $SESSION
