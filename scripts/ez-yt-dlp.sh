bb20bb4f1f898971388b82318ba188f24ee425ea
KTN2757 <80688932+KTN2757@users.noreply.github.com>
2026-06-21 17:38:00 +0545

    Fixed ez-yt-dlp script

--- scripts/ez-yt-dlp.sh
+++ scripts/ez-yt-dlp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ $# -eq 3 ]; then
-    yt-dlp "$1" -o "foo.webm" --cookies-from-browser firefox
+    yt-dlp "$1" -o "foo.webm" --use-extractors youtube --extractor-args "youtube:player_client=web" --cookies-from-browser firefox || { echo "Download failed"; exit 1; }
     ffmpeg -i foo.webm "$2.mp3"
     mv "./$2.mp3" "$HOME/Music/$3"
     rm "foo.webm"
 fi
 if [ $# -eq 4 ]; then
-    yt-dlp "$1" -o "foo.webm" --cookies-from-browser firefox
+    yt-dlp "$1" -o "foo.webm" --use-extractors youtube --extractor-args "youtube:player_client=web" --cookies-from-browser firefox || { echo "Download failed"; exit 1; }
     ffmpeg -i foo.webm "$2.$3"
     mv "./$2.$3" "$HOME/Music/$4"
     rm "foo.webm"
