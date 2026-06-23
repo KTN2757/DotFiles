939ebffd7b1db9bc5842a5d5c237aaaa3303abb6
KTN2757 <80688932+KTN2757@users.noreply.github.com>
2026-06-23 09:07:52 +0545

    Fixed Music session getting interrupted

--- tmux/clear-out-everything.sh
+++ tmux/clear-out-everything.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-tmux list-panes -a -F '#{pane_id}' | while read pane; do
+tmux list-panes -a -F '#{session_name}:#{pane_id}' | while read line; do
+    session="${line%%:*}"
+    pane="${line##*:}"
+    [[ "$session" == "Music" ]] && continue
     tmux send-keys -t "$pane" 'reset' Enter
 done
